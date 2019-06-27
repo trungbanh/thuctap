0d1cd3be87da48a6071b3d1f8c8b0b800c1f4a1c
Nguyễn Thiện Hùng <nthung@ninepoints.vn>
2019-06-27 17:34:45 +0700

    - Fix remove function magic: __get, __set.

--- Model/AbstractModel.php
+++ Model/AbstractModel.php
@@ -10,35 +10,31 @@ abstract class AbstractModel {
         $this->data = $data;
     }
 
-    public function __get($varname) {
-        if (isset($this->data[$varname])) {
-            return $this->data[$varname];
-        }
-
-        return null;
-    }
-    
-    public function __set($varname, $value) {
-        return $this->data[$varname] = $value;
-    }
     /**
      * Set/Get attribute wrapper
      *
-     * @param   string $method
-     * @param   array $args
+     * @param string $method
+     * @param array $args
+     *
      * @return  mixed
+     * @throws \Exception
      */
     public function __call($method, $args)
     {
         switch (substr($method, 0, 3)) {
             case 'get' :
                 $key = $this->_underscore(substr($method,3));
-                $data = $this->{$key};
+                if (isset($this->data[$key])) {
+                    $data = $this->data[$key];
+                } else {
+                    $data = null;
+                }
+
                 return $data;
 
             case 'set' :
                 $key = $this->_underscore(substr($method,3));
-                return $this->{$key} = isset($args[0]) ? $args[0] : null;
+                return $this->data[$key] = isset($args[0]) ? $args[0] : null;
 
             case 'uns' :
                 $key = $this->_underscore(substr($method,3));
@@ -55,13 +51,13 @@ abstract class AbstractModel {
                 $key = $this->_underscore(substr($method,3));
                 return isset($this->_data[$key]);
         }
-        throw new Varien_Exception("Invalid method ".get_class($this)."::".$method."(".print_r($args,1).")");
+
+        throw new \Exception(\sprintf('Invalid method %s::%s(%s)', \get_class($this), $method, \print_r($args, 1)));
     }
 
     protected function _underscore($name)
     {
-        $result = strtolower(preg_replace('/(.)([A-Z])/', "$1_$2", $name));
-        return $result;
+        return \strtolower(\preg_replace('/(.)([A-Z])/', '$1_$2', $name));
     }
 }
 
